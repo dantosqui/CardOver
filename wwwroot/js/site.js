413bff332e01e7f5992fc7129a37d1c048aba67c
TOMAS FERNANDEZ ZABALA <47394082@ort>
2023-11-06 10:35:28 -0300

    comillas<

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-
+﻿
 function TirarCarta(idCarta, jugador){
 
 const cartaid= jugador + ' ' + idCarta
@@ -11,7 +6,11 @@ const cartaid= jugador + ' ' + idCarta
 const carta = document.getElementById(cartaid)
 console.log(carta)
 
-document.getElementById('divdelmedio').appendChild(carta)
+
+const divdelmedio=document.getElementById('divdelmedio')
+
+divdelmedio.appendChild(carta)
+
 
 }
 
