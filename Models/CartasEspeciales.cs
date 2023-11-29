fea6821cd852b05238a0a22ef9fcc146eb2cba65
Zareke <47696115@est.ort.edu.ar>
2023-11-28 23:58:53 -0300

    Casi finalizado

--- Models/CartasEspeciales.cs
+++ Models/CartasEspeciales.cs
@@ -2,7 +2,7 @@ public class CartasEspeciales{ //fijate si es como en la bd a espera me puedo fi
     public int IdCarta{get;set;}
     public string Nombre {get;set;}
     public string NombreHabilidad {get;set;}
-    public string DescripcionHabilidad {get;set;}
+    public string tipoCarta {get;set;}
     public int PuntosAtaque {get;set;}
     public int PuntosDefensa {get;set;}
 } 	
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,6 +2,7 @@
 var constVida = 5000
 var vida1 = 100
 var vida2 = 100
+var vivos=true
 var ismuertesubita=false;
 function NoLocalStorage(){
     localStorage.setItem("vida1",100)
@@ -25,9 +26,11 @@ function LocalStorage(){
     const perdiojug1=vida1<=0
         const perdiojug2=vida2<=0
         if(perdiojug1 && perdiojug2){
-            $('#idModal1').modal('show')
-        
-            ismuertesubita=true
+            console.log("AAA")      
+            GameOver(3)
+               // $('#idModal1').modal('show')
+            
+                ismuertesubita=true
         } 
         else if (perdiojug1){
             GameOver(2)
@@ -45,21 +48,24 @@ function TirarCarta(idCarta, jugador,puedeTirar){
     console.log("tirar carta error")
     const carta = document.getElementById(idCarta)
     let cartaTirada 
-
-    if(idCarta <4)
+    let divdelmedio
+    if(idCarta <4){
         cartaTirada = document.getElementById('cartaTirada1')
-    else
+        divdelmedio=document.getElementById('divdelmedio1')}
+    else{
         cartaTirada = document.getElementById('cartaTirada2')
+        divdelmedio=document.getElementById('divdelmedio2')}
         const lugarCarta = "lugarCarta_" + idCarta
-        const divdelmedio=document.getElementById('divdelmedio')
-    if(puedeTirar.innerHTML == "verdadero" && vida1!=0 && vida2!=0){
+        
+    if(puedeTirar.innerHTML == "verdadero" && vivos){
         const tirarAudio = new Audio('https://dl.vgmdownloads.com/soundtracks/plants-vs.-zombies-2009-gamerip-pc-ios-x360-ps3-ds-android-mobile-psvita-xbox-one-ps4-switch/yytiqdghnq/SFX%20paper.mp3')
         tirarAudio.play()
         divdelmedio.appendChild(carta)
         puedeTirar.innerHTML = "falso"
         cartaTirada.innerHTML = idCarta
-    }else if (idCarta == cartaTirada.innerHTML){
-        
+    }else if (idCarta == cartaTirada.innerHTML && vivos){
+        console.log(vivos)
+        console.log("cartita")
         document.getElementById(lugarCarta).appendChild(carta)
         puedeTirar.innerHTML = "verdadero"
     }
@@ -75,10 +81,10 @@ function TirarDado(){
     const rnd2 = Math.floor(Math.random() * 5);
     console.log(rnd1 + "DADO 1 RESULTADO DADO")
 
-    const divdelmedio=document.getElementById("divdelmedio")
-    const cartas=divdelmedio.querySelectorAll(".card")
-    const carta1=cartas[0]
-    const carta2=cartas[1]
+    const divdelmedio1=document.getElementById("divdelmedio1")
+    const divdelmedio2=document.getElementById("divdelmedio2")
+    const carta1=divdelmedio1
+    const carta2=divdelmedio2
     
     
     const danio1 = rnd1*carta1.querySelectorAll(".danio")[0].innerHTML
@@ -87,17 +93,27 @@ function TirarDado(){
     const defensa2 = rnd2*carta2.querySelectorAll(".defenza")[0].innerHTML
     let danioRecibeTotal1
     let danioRecibeTotal2
-    
-    if(danio2-defensa1>=0)
+    console.log(localStorage.getItem("vida1") + "VIDA 1 ")
+    if(danio2-defensa1>0 && danio2-defensa1<localStorage.getItem("vida1"))
     danioRecibeTotal1 = danio2-defensa1
-    else
-    danioRecibeTotal1 = 0
+    else{
+        if(danio2-defensa1 <=0){
+            danioRecibeTotal1=0
+        }else
+        danioRecibeTotal1 = (vida1/100)*constVida
+    }
 
 
-    if(danio1-defensa2>=0)
+
+    if(danio1-defensa2 > 0 && danio1-defensa2<localStorage.getItem("vida2"))
     danioRecibeTotal2 = danio1-defensa2
-    else
-    danioRecibeTotal2 = 0
+    else{
+        if(danio1-defensa2 <= 0){
+            danioRecibeTotal2 = 0
+        }else
+        danioRecibeTotal2 = (vida2/100)*constVida
+    }
+ 
 
     if (!ismuertesubita)
     {
@@ -106,14 +122,12 @@ function TirarDado(){
 
         document.getElementById("spannum1").innerHTML=rnd1
         document.getElementById("spannum2").innerHTML=rnd2
-        document.getElementById("spandanio1").innerHTML=danio1
-        document.getElementById("spandanio2").innerHTML=danio2
         document.getElementById("spandefensa1").innerHTML=defensa1
         document.getElementById("spandefensa2").innerHTML=defensa2
-
-        document.getElementById("dañototal1").innerHTML=-danioRecibeTotal1
-        document.getElementById("dañototal2").innerHTML=-danioRecibeTotal2   
-
+console.log(danioRecibeTotal1 + "AAAAAAAAAAAAA")
+console.log(danioRecibeTotal2 + "AAAAAAAAAAAAA")
+        document.getElementById("dañototal1").innerHTML="-" +danioRecibeTotal1
+        document.getElementById("dañototal2").innerHTML="-" + danioRecibeTotal2   
 
 
         /*NO ME SALE LO TERMINO DESPUES */ //salio o no?? parece que anda -dante
@@ -132,9 +146,10 @@ function TirarDado(){
   
 
 
-    if(perdiojug1 && perdiojug2){       
-    
-        $('#idModal1').modal('show')
+    if(perdiojug1 && perdiojug2){ 
+        console.log("AAA")      
+    GameOver(0)
+       // $('#idModal1').modal('show')
     
         ismuertesubita=true
     }
@@ -174,6 +189,7 @@ const canciones = [
 "https://vgmsite.com/soundtracks/plants-vs.-zombies/rqnczgecuc/10.%20Ultimate%20Battle.mp3"
 ]
 function Soundtrack(mucara){
+   
 const id = canciones.findIndex(x => x == mucara.src)
 if(id <3) mucara.src = canciones[id+1]
 else mucara.src = canciones[0]
@@ -254,9 +270,18 @@ console.log("aaaa")
 //bien pensado woody :v V:p
 function GameOver(jug){
     console.log("gameover corrido")
+    vivos=false
 
     document.getElementById("gameover").hidden = false;
    // window.location.assign("GameOver?jug="+jug)
+   const ganador = document.getElementById("ganador")
+   if(jug==0){
+    ganador.innerHTML= "Empate"
+   }else if(jug==1){
+    ganador.innerHTML="Gana jugador 1"
+   }else{
+    ganador.innerHTML="Gana jugador 2"
+   }
     NoLocalStorage()
     
 
@@ -273,7 +298,12 @@ function Recargar() {
 location.reload();
 }
 function Empezar(uno,dos){
-    LocalStorage()
+if(localStorage.length == 0){
+        NoLocalStorage()
+   }else{
+        LocalStorage()
+   }
+   
 }
 /*
 function GuardarCartasStorage(uno,dos){
